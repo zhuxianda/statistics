c28662f7694c9734dcbced362a8d0d155e6c3a33
zhu <619127324@qq.com>
2019-05-24 11:51:47 +0800

    1

--- src/main/java/com/shagnzhi/statisticsDemo/StatisticsDemoApplication.java
+++ src/main/java/com/shagnzhi/statisticsDemo/StatisticsDemoApplication.java
@@ -3,11 +3,15 @@ package com.shagnzhi.statisticsDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class StatisticsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StatisticsDemoApplication.class, args);
+		System.out.println("输出启动参数:");
+		System.out.println("参数"+Arrays.toString(args));
 	}
 
 }
