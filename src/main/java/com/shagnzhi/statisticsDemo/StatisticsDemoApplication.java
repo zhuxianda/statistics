0b4271f53e87f6b281a62b45150a7935293fbf60
zhu <619127324@qq.com>
2019-05-24 11:04:37 +0800

    第一次初始化

---
+++ src/main/java/com/shagnzhi/statisticsDemo/StatisticsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shagnzhi.statisticsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticsDemoApplication.class, args);
+	}
+
+}
